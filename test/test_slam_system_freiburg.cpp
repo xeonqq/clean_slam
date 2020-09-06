977390b64692ea0a2f250c9a4da1481436b62726
xeonqq <xeonqq@gmail.com>
2020-09-06 15:40:46 +0000

    Calculate KPI of slam result

--- test/test_slam_system_freiburg.cpp
+++ test/test_slam_system_freiburg.cpp
@@ -27,19 +27,50 @@ bool operator==(const g2o::SE3Quat &lhs, const g2o::SE3Quat &rhs) {
 
 float ComputeTransformationError(const g2o::SE3Quat &ground_truth,
                                  const StampedTransformation &calculated) {
-  const auto calculated_rotation =
-      calculated.GetTransformation().to_homogeneous_matrix().block<3, 3>(0, 0);
-  const auto calculated_euler_angle = calculated_rotation.eulerAngles(0, 1, 2);
   const auto calculated_translation =
       calculated.GetTransformation().to_homogeneous_matrix().block<3, 1>(0, 3);
 
-  const auto gt_rotation =
-      ground_truth.to_homogeneous_matrix().block<3, 3>(0, 0);
-  const auto gt_euler_angle = gt_rotation.eulerAngles(0, 1, 2);
   const auto gt_translation =
       ground_truth.to_homogeneous_matrix().block<3, 1>(0, 3);
   return (gt_translation - calculated_translation).norm();
 }
+
+float ComputeRotationError(const g2o::SE3Quat &ground_truth,
+                           const StampedTransformation &calculated) {
+  const auto calculated_rotation = calculated.GetTransformation().rotation();
+  const auto gt_rotation = ground_truth.rotation();
+  auto diff = gt_rotation.angularDistance(calculated_rotation);
+  return diff;
+}
+
+struct Report {
+  void CalculateError(const g2o::SE3Quat &gt_initial_pose_wrt_current_pose,
+                      const StampedTransformation &stamped_transformation) {
+    const auto translation_error = ComputeTransformationError(
+        gt_initial_pose_wrt_current_pose, stamped_transformation);
+    _translation_errors.push_back(translation_error);
+    _translation_error_sum += translation_error;
+
+    const auto rotation_error = ComputeRotationError(
+        gt_initial_pose_wrt_current_pose, stamped_transformation);
+    _rotation_errors.push_back(rotation_error);
+    _rotation_error_sum += rotation_error;
+    ++_count;
+  }
+
+  float GetMeanTranslationError() { return _translation_error_sum / _count; }
+  float GetMeanRotationError() { return _rotation_error_sum / _count; }
+  void show() {
+    std::cout << "Report: \ntrajectory size: " << _count << "\n"
+              << "mean translate error: " << GetMeanTranslationError() << "\n"
+              << "mean rotation error: " << GetMeanRotationError() << std::endl;
+  }
+  int _count{0};
+  std::vector<double> _rotation_errors;
+  std::vector<double> _translation_errors;
+  float _translation_error_sum;
+  float _rotation_error_sum;
+};
 
 TEST(SlamSystemFreiburgTest, Run) {
   // arrange
@@ -54,57 +85,26 @@ TEST(SlamSystemFreiburgTest, Run) {
 
   // assert
   const auto &trajectory = system.GetCamTrajectory();
-  std::cout << "trajectory size: " << trajectory.size() << std::endl;
-  //  EXPECT_EQ(trajectory.size(), 2);
 
   const auto initial_gt_transformation =
       dataset_loader.GetGroundTruthAt(trajectory.front().GetTimestamp())
           .GetTransformation();
+
+  Report report;
   for (const auto &stamped_transformation : trajectory) {
     const g2o::SE3Quat ground_truth_transformation =
         dataset_loader.GetGroundTruthAt(stamped_transformation.GetTimestamp())
             .GetTransformation();
     const auto gt_initial_pose_wrt_current_pose =
         ground_truth_transformation * initial_gt_transformation.inverse();
-    //    std::printf("slam time %.6f\n",
-    //    stamped_transformation.GetTimestamp());
-    std::cout << "slam result: \n"
-              << stamped_transformation.GetTransformation() << std::endl;
-    const auto rotation = stamped_transformation.GetTransformation()
-                              .to_homogeneous_matrix()
-                              .block<3, 3>(0, 0);
-    const auto euler_angles = rotation.eulerAngles(0, 1, 2);
-    const auto translation = stamped_transformation.GetTransformation()
-                                 .to_homogeneous_matrix()
-                                 .block<3, 1>(0, 3);
 
-    std::cout << "ground truth: \n"
-              << gt_initial_pose_wrt_current_pose << std::endl;
-    std::cout << "translation error: \n"
-              << ComputeTransformationError(gt_initial_pose_wrt_current_pose,
-                                            stamped_transformation)
-              << std::endl;
-    std::cout << "euler angle calculated: \n";
-    std::cout << euler_angles << std::endl;
-
-    const auto rotation_gt =
-        gt_initial_pose_wrt_current_pose.to_homogeneous_matrix().block<3, 3>(0,
-                                                                             0);
-    const auto translation_gt =
-        gt_initial_pose_wrt_current_pose.to_homogeneous_matrix().block<3, 1>(0,
-                                                                             3);
-    const auto euler_angles_gt = rotation_gt.eulerAngles(0, 1, 2);
-
-    //    float scale = 5.524032482088845;
-    //    for (size_t i = 0; i < 3; ++i) {
-    //      EXPECT_NAER(euler_angles_gt[i], euler_angles[i], );
-    //      EXPECT_NEAR(translation_gt[i], translation[i], 1e-5);
-    //    }
-    std::cout << "euler angle gt: \n";
-    std::cout << euler_angles_gt << std::endl;
-    std::cout << std::endl;
+    report.CalculateError(gt_initial_pose_wrt_current_pose,
+                          stamped_transformation);
   }
-  //  EXPECT_EQ(trajectory[0], groundtruths[0].GetTransformation());
+
+  report.show();
+  EXPECT_LE(report.GetMeanTranslationError(), 0.1655865);
+  EXPECT_LE(report.GetMeanRotationError(), 0.1421);
 }
 
 } // namespace clean_slam
